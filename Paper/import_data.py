52a0aacc7f293fdd0878862cebc9d07962f4c709
Aragorn-64 <advaysagarkar@gmail.com>
2024-04-02 05:21:17 +0530

    refactor

---
+++ Paper/import_data.py
@@ -0,0 +1,33 @@
+import os
+import cv2
+import tensorflow as tf
+import numpy as np
+from typing import List
+from matplotlib import pyplot as plt
+import imageio
+
+tf.config.list_physical_devices('GPU')
+
+physical_devices = tf.config.list_physical_devices('GPU')
+try:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+except:
+    pass
+
+vocab = [x for x in "abcdefghijklmnopqrstuvwxyz'?!123456789 "]
+
+char_to_num = tf.keras.layers.StringLookup(vocabulary=vocab, oov_token="")
+num_to_char = tf.keras.layers.StringLookup(
+    vocabulary=char_to_num.get_vocabulary(), oov_token="", invert=True
+)
+
+
+def load_alignments(path: str) -> List[str]:
+    with open(path, 'r') as f:
+        lines = f.readlines()
+    tokens = []
+    for line in lines:
+        line = line.split()
+        if line[2] != 'sil':
+            tokens = [*tokens, ' ', line[2]]
+    return char_to_num(tf.reshape(tf.strings.unicode_split(tokens, input_encoding='UTF-8'), (-1)))[1:]
